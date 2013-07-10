62ddddcc72de5a371279ab99b0643c549b5fe64f
Yinhuan Yuan <yhyuan@gmail.com>
2013-07-09 22:00:08 -0400

    Finish grader.js

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://nameless-peak-3516.herokuapp.com/"
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +36,17 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertURLExists = function(url) {
+	rest.get(url).on('complete', function(result) {
+	    if (result instanceof Error) {
+                 console.log("%s is not a valid URL.", url);
+                 process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	     } else {
+	          return result;
+	      }
+       });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,7 +78,9 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to index.html', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
+   var htmlfile = program.hasOwnProperty('file')? program.file : program.url;
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
